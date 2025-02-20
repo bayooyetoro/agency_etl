3de6280bbe338fc62117c5de69693bb7c609fe4c
Bayo <bayo@BayoPC.>
2025-02-20 23:46:30 +0100

    done

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ logger = setup_logger("my_logger", "main.log", filemode='w')
 
 
 # file paths
-json_file_name = "./data/raw_data.json"
+json_file_path = "./data/raw_data.json"
 csv_file_path = "./data/extracted_data.csv"
 url = "https://restcountries.com/v3.1/all"
 
@@ -14,10 +14,10 @@ url = "https://restcountries.com/v3.1/all"
 def main():
     try:
         logger.info("getting data from the API...")
-        get_data(url, json_file_name)
+        get_data(url, json_file_path)
 
         logger.info("File Saved Successfully! Starts Extraction...")
-        extract_countries_data(json_file_name)
+        extract_countries_data(json_file_path)
 
         logger.info("Extraction Completed! Transforming and loading to DB...")
         load_data_to_db(csv_file_path)
